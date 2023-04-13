f39e0d86a641a2dd36e3ce0b788efc0db1b570c7
RubenSmetsPXL <Ruben.Smets@student.pxl.be>
2023-04-13 18:19:58 +0200

    moved image and video folders

--- src/components/About.js
+++ src/components/About.js
@@ -2,16 +2,18 @@ import React from 'react';
 import '../App.css'
 import './About.css';
 import './VideoSection.css';
+import code from "./videos/code.mp4";
+import ik from "./images/ik.jpg";
 
 function AboutSection() {
   return (
     <>
         <div className='video-container'>
-        <video src='/videos/code.mp4' autoPlay loop muted />
+        <video src={code} autoPlay loop muted />
         </div>
         <div className='about-container'>
             <h1>Over mij</h1>
-            <img src='/images/ik.jpg' alt='ik' className='photo'/>
+            <img src={ik} alt='ik' className='photo'/>
             <div className='text'>
                 <p> &gt; Mijn naam is Ruben Smets;</p>
                 <p> &gt; Ik ben 22 jaar oud en studeer AI & Robotics;</p>
--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -2,11 +2,12 @@ import React from 'react';
 import '../App.css'
 import '../App.css';
 import './VideoSection.css';
+import helloworld from './videos/hello-world.mp4'
 
 function VideoSection() {
   return (
     <div className='video-container'>
-      <video src='/videos/hello-world.mp4' autoPlay loop muted />
+      <video src={helloworld} autoPlay loop muted />
     </div>
   );
 }
